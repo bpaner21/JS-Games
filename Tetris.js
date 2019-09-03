061e46eaa57dc9697cf1d118d0efa0f959d9417c
B.J. Paner <bpaner@jellyvision.com>
2019-09-03 17:46:52 -0500

    Finished Grid setup
    See Changelog.txt

--- Tetris.js
+++ Tetris.js
@@ -1,38 +1,38 @@
 // Variables
-{
-    // Debug switch
-    let debug = true;
 
-    // Reference HTML file
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
+// Debug switch
+let debug = true;
 
-    // The distance from the screen edge in pixels
-    const offset = 20;
+// Reference HTML file
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-    // The side length of a square in pixels
-    // Not constant so the app can scale to screen size
-    let size = 12;
+// The side length of a square in pixels
+// Not constant so the app can scale to screen size
+let size = 13;
 
-    // The basic square that defines the grid, tetris blocks
-    const Square = class{
-        constructor(fill, color, x, y){
-            this.fill = fill;
-            this.color = color;
-            this.x = x;
-            this.y = y;
-        }
+// The distance from the screen edge in pixels
+const offset = size * 2; 
+
+// The basic square that defines the grid, tetris blocks
+const Square = class{
+    constructor(fill, color, x, y){
+        this.fill = fill;
+        this.color = color;
+        this.x = x;
+        this.y = y;
     }
 }
+
 
 // Setup
 {
     // Create the playing grid
     let Grid = [];
-    for (let i = 0; i < 10; ++i){
-        Grid[i] = [];
-        for (let j = 0; j < 22; ++j){
-            Grid[i][j] = new Square(false, "#FF00FF", offset + (i *size), (j *size));
+    for (let x = 0; x < 10; ++x){
+        Grid[x] = [];
+        for (let y = 0; y < 22; ++y){
+            Grid[x][y] = new Square(false, "#FF00FF", offset + (x * size), (y * size));
         }
     }
 
@@ -40,17 +40,17 @@
         context.rect(offset, offset, (size * 10), (size * 20));
         context.stroke();
 
-        for (let i = 30; i < 120; i += 10){
+        for (let x = (offset + size); x < (offset + (size * 10)); x += size){
             context.beginPath();
-            context.moveTo(i, 20);
-            context.lineTo(i, 220);
+            context.moveTo(x, offset);
+            context.lineTo(x, offset + (size * 20));
             context.stroke();
         }
 
-        for (let j = 30; j < 220; j += 10){
+        for (let y = (offset + size); y < (offset + (size * 20)); y += size){
             context.beginPath();
-            context.moveTo(20, j);
-            context.lineTo(120, j);
+            context.moveTo(offset, y);
+            context.lineTo(offset + (size * 10), y);
             context.stroke();
         }
 
@@ -59,26 +59,37 @@
         context.rect(offset, 0, (size * 10), (size * 22));
         context.stroke();
         
-        for (let i = 30; i < 120; i += 10){
+        for (let x = (offset + size); x < (offset + (size * 10)); x += size){
             context.beginPath();
-            context.moveTo(i, 0);
-            context.lineTo(i, 220);
+            context.moveTo(x, 0);
+            context.lineTo(x, (size * 22));
             context.stroke();
         }
 
-        for (let j = 10; j < 220; j += 10){
+        for (let y = size; y < (size * 22); y += size){
             context.beginPath();
-            context.moveTo(20, j);
-            context.lineTo(120, j);
+            context.moveTo(offset, y);
+            context.lineTo(offset + (size * 10), y);
             context.stroke();
         }
     }
 
-    for (let i = 0; i < 10; ++i){
-        for (let j = 2; j < 22; ++j){
-            Grid[i][j].fill = true;
-            context.fillStyle = Grid[i][j].color;
-            context.fillRect(Grid[i][j].x + 2, Grid[i][j].y + 2, size - 4, size - 4);
+    /* for (let x = 0; x < 10; ++x){
+        if (!debug) {
+            for (let y = 0; y < 22; ++y){
+                Grid[x][y].fill = true;
+                context.fillStyle = Grid[x][y].color;
+                context.fillRect(Grid[x][y].x + 2, Grid[x][y].y + 2, size - 4, size - 4);
+            }
         }
-    }
+        else {
+            for (let y = 0; y < 22; ++y){
+                Grid[x][y].fill = true;
+                context.fillStyle = Grid[x][y].color;
+                context.fillRect(Grid[x][y].x + 2, Grid[x][y].y + 2, size - 4, size - 4);
+            }
+        }
+    } //*/
+
+
 }
